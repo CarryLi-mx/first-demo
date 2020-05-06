3b6c07d6eebaa73a777fdcb89142d64a1718a2e4
carryLi-mx <bnb9999@outlook.com>
2020-05-06 18:37:15 +0800

    demo

--- testgit/src/main/java/com/jiawang/Test.java
+++ testgit/src/main/java/com/jiawang/Test.java
@@ -2,6 +2,8 @@ package com.jiawang;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        String s = "llmmxx";
+        String substring = s.substring(1,3);
+        System.out.println(substring);
     }
 }
